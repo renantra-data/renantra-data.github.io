d68623dfca31fa535a2c890ae4b3b813303b3db9
Rendi Andika Putra <rendiandikaputrarenantradata@gmail.com>
2026-01-31 09:51:04 +0700

    commit le

--- script_satu.js
+++ script_satu.js
@@ -1,6 +1,22 @@
-// interaksi sederhana
-document.querySelectorAll("nav a").forEach(link => {
-  link.addEventListener("click", () => {
-    console.log("Navigasi diklik:", link.textContent);
+const links = document.querySelectorAll("nav a");
+const pages = document.querySelectorAll(".page");
+
+links.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+
+    const target = link.dataset.page;
+
+    // menu active
+    links.forEach(l => l.classList.remove("active"));
+    link.classList.add("active");
+
+    // page switch
+    pages.forEach(page => {
+      page.classList.remove("active");
+      if (page.id === target) {
+        page.classList.add("active");
+      }
+    });
   });
 });
