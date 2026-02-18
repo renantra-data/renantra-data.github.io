c92ffc953ac6a2f559f0af1a52ce42d8f6ee44ca
Rendi Andika Putra <rendiandikaputrarenantradata@gmail.com>
2026-02-18 16:22:46 +0700

    Delete script_satu.js

--- script_satu.js
+++
@@ -1,8 +0,0 @@
-(function(){
-    const btn = document.getElementById('menuToggle');
-    const nav = document.getElementById('mainNav');
-    btn.addEventListener('click', function(){
-        const open = nav.classList.toggle('open');
-        btn.setAttribute('aria-expanded', open);
-    });
-})();
